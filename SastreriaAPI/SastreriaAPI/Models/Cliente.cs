fca57bc31520553814a3c583c12d5a8454332e55
Jairo <calderonsebas1102@hotmail.com>
2026-04-30 23:21:41 -0500

    Add files via upload

---
+++ SastreriaAPI/SastreriaAPI/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SastreriaAPI.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Telefono { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
+
+        public string Direccion { get; set; } = string.Empty;
+    }
+}
